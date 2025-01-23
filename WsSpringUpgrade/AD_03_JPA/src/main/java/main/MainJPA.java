22203d78786bdb44afe8b4a32576f976d320c337
Carlos Gómez <carlosgomzp21@gmail.com>
2025-01-23 19:42:29 +0100

    añadido ejercicio JPA

---
+++ WsSpringUpgrade/AD_03_JPA/src/main/java/main/MainJPA.java
@@ -0,0 +1,13 @@
+package main;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class MainJPA {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("PruebaJPA");
+		EntityManager em = emf.createEntityManager();
+		em.close();
+}
+}
